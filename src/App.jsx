46ad11a02f6ae6f660a070fc7f7e9929ffe35869
Jesse Hernandez <jessebubble@jesses-air.attlocal.net>
2023-09-10 15:32:30 -0500

    components and layout setup for examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,52 @@
+import { PageIntro } from './components/PageIntro';
+import { AnimationPatterns } from './components/examples/animation-patterns/AnimationPatterns';
+import { ClipBoardPatterns } from './components/examples/clipboard-patterns/ClipBoardPatterns';
+import { ComponentPatterns } from './components/examples/component-patterns/ComponentPatterns';
+import { FilesDirectoriesPatterns } from './components/examples/files-directories-patterns/FilesDirectoriesPatterns';
+import { LayoutPatterns } from './components/examples/layout-patterns/LayoutPatterns';
+import { MediaPatterns } from './components/examples/media-patterns/MediaPatterns';
+import { ThemingPatterns } from './components/examples/theming-patterns/ThemingPatterns';
+
+function Footer() {
+    return (
+        <footer className="mt-24 w-full sm:mt-32">
+            <div className="border-t border-neutral-950/10 pr-3 pt-12">
+                <p className="text-end text-sm text-gray-700">
+                    <a
+                        className="hover:text-centro-pink"
+                        href="https://www.jessebubble.dev/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        jessebubble.dev <span aria-hidden="true">→</span>
+                    </a>
+                </p>
+            </div>
+        </footer>
+    );
+}
+
 export default function App() {
     return (
         <>
-            <h1 className="text-9xl text-centro-blue">Patterns</h1>
+            <PageIntro
+                eyebrow="Patterns"
+                title="Breaking down common web design and development UI components and patterns"
+            >
+                <p>
+                    Patterns is a collection of web design and development techniques aimed to optimize the online experience with consideration for accessibility and user preferences
+                </p>
+            </PageIntro>
+            
+            <AnimationPatterns />
+            <ClipBoardPatterns />
+            <ComponentPatterns />  
+            <FilesDirectoriesPatterns /> 
+            <LayoutPatterns />
+            <MediaPatterns />
+            <ThemingPatterns />
+
+            <Footer />
         </>
     );
 }
---
+++ src/components/examples/media-patterns/MediaPatterns.jsx
@@ -0,0 +1,50 @@
+import { Container } from '../../Container';
+import { Border } from '../../Border';
+import { FadeIn } from '../../FadeIn';
+import { VideoCameraIcon } from '@heroicons/react/24/solid';
+
+export function MediaPatterns() {
+    return (
+        <>
+            <Container className="">
+                <div className="mt-10 space-y-20 sm:space-y-24 lg:space-y-32">
+                    <FadeIn>
+                        <article>
+                            <Border className="grid grid-cols-3 gap-x-8 gap-y-8 pt-16">
+                                <div className="col-span-full sm:flex sm:items-center sm:justify-between sm:gap-x-8 lg:col-span-1 lg:block">
+                                    <div className="sm:flex sm:items-center sm:gap-x-6 lg:block">
+                                        <VideoCameraIcon className="h-12 w-12 flex-none text-neutral-950" />
+                                        <h3 className="mt-6 flex items-center text-sm font-semibold text-neutral-950 sm:mt-0 lg:mt-8">
+                                            Modern Media Experiences
+                                        </h3>
+                                    </div>
+                                    <div className="mt-1 flex gap-x-4 sm:mt-0 lg:block">
+                                        <p className="text-sm tracking-tight text-neutral-950 after:ml-4 after:font-semibold after:text-neutral-300 after:content-['/'] lg:mt-2 lg:after:hidden">
+                                            Picture in Picture 
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="col-span-full lg:col-span-2 lg:max-w-2xl">
+                                    <p className="font-display text-4xl font-medium text-neutral-950">
+                                        Media Patterns
+                                    </p>
+                                    <div className="mt-6 space-y-6 text-base text-neutral-600">
+                                        <p>
+                                            Welcome to our growing collection of patterns for working with media. These patterns demonstrate considerations for accessibility, user preferences 
+                                            and support all modern browsers
+                                        </p>
+                                    </div>
+                                    <div className="mt-8 flex">
+                                        <div className="flex w-full items-center justify-center rounded-lg border bg-cosa-white p-2">
+                                            
+                                        </div>                                        
+                                    </div>
+                                </div>
+                            </Border>
+                        </article>
+                    </FadeIn>
+                </div>
+            </Container>
+        </>
+    );
+}
